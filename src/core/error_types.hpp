c58bf06719dd32de2868d2b0a39d534b4402351f
Martin Feichtinger <martin.feichtinger22@gmail.com>
2026-07-26 21:43:36 +0200

    changed PIXEL_OUT_OF_BOUNDS error level to WARNING and added CRASH_ON_WARNING mode

--- src/core/error_types.hpp
+++ src/core/error_types.hpp
@@ -11,12 +11,13 @@ enum class ErrorLevel : std::uint8_t {
 };
 
 enum class ErrorMode : std::uint8_t {
-    SILENT = 0,			// no reaprintction
-    LOG_ONLY = 1,		// only log errors
-	LOG_LIVE = 2,		// log errors an print them in service mode
-	BRODCAST = 3,		// log and brodcast even when service mode is deactivated		
-    CRASH_ON_FATAL = 4,	// brodcast error, set visual display info and halt the system at fatal error
-	CRASH_ON_ERROR = 5	// -||- at normal error
+    SILENT = 0,				// no reaprintction
+    LOG_ONLY = 1,			// only log errors
+	LOG_LIVE = 2,			// log errors an print them in service mode
+	BRODCAST = 3,			// log and brodcast even when service mode is deactivated		
+    CRASH_ON_FATAL = 4,		// brodcast error, set visual display info and halt the system at fatal error
+	CRASH_ON_ERROR = 5,		// -||- at error
+	CRASH_ON_WARNING = 6	// -||- at warning
 };
 
 enum class ErrorCode : std::uint8_t {
@@ -34,7 +35,7 @@ inline const char* getErrorMessage(ErrorCode code) {
 
 inline ErrorLevel getErrorLevel(ErrorCode code) {
     switch (code) {
-        case ErrorCode::PIXEL_OUT_OF_BOUNDS:	return ErrorLevel::ERROR;
+        case ErrorCode::PIXEL_OUT_OF_BOUNDS:	return ErrorLevel::WARNING;
         default:								return ErrorLevel::INFO;
     }
 }
@@ -42,24 +43,25 @@ inline ErrorLevel getErrorLevel(ErrorCode code) {
 // translate error level to string for serial output
 inline const char* levelToString(ErrorLevel level) {
     switch (level) {
-        case ErrorLevel::INFO:			return "INFO";
-        case ErrorLevel::WARNING:		return "WARN";
-        case ErrorLevel::ERROR:			return "ERROR";
-        case ErrorLevel::FATAL:			return "FATAL";
-        default:						return "UNKN";
+        case ErrorLevel::INFO:				return "INFO";
+        case ErrorLevel::WARNING:			return "WARN";
+        case ErrorLevel::ERROR:				return "ERROR";
+        case ErrorLevel::FATAL:				return "FATAL";
+        default:							return "UNKN";
     }
 }
 
 // translate error mode to string for serial output
 inline const char* modeToString (ErrorMode mode) {
 	switch (mode) {
-		case ErrorMode::SILENT:			return "SILENT";
-		case ErrorMode::LOG_ONLY:		return "LOG_ONLY";
-		case ErrorMode::LOG_LIVE:		return "LOG_LIVE";
-		case ErrorMode::BRODCAST:		return "BRODCAST";
-		case ErrorMode::CRASH_ON_FATAL:	return "CRASH_ON_FATAL";
-		case ErrorMode::CRASH_ON_ERROR:	return "CRASH_ON_ERROR";
-		default:						return "UNKN";
+		case ErrorMode::SILENT:				return "SILENT";
+		case ErrorMode::LOG_ONLY:			return "LOG_ONLY";
+		case ErrorMode::LOG_LIVE:			return "LOG_LIVE";
+		case ErrorMode::BRODCAST:			return "BRODCAST";
+		case ErrorMode::CRASH_ON_FATAL:		return "CRASH_ON_FATAL";
+		case ErrorMode::CRASH_ON_ERROR:		return "CRASH_ON_ERROR";
+		case ErrorMode::CRASH_ON_WARNING:	return "CRASH_ON_WARNING";
+		default:							return "UNKN";
 	}
 }
 
